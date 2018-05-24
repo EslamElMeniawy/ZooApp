548ad5cdd57b7bb8416177ce51da1fc75011d87a
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2018-05-24 10:26:06 +0200

    Design animals list

---
+++ app/src/main/java/elmeniawy/eslam/zoo/Animal.kt
@@ -0,0 +1,13 @@
+package elmeniawy.eslam.zoo
+
+/**
+ * Animal
+ *
+ * Created by Eslam El-Meniawy on 24-May-2018.
+ * CITC - Mansoura University
+ */
+class Animal(name: String, description: String, image: Int) {
+    var name: String? = name
+    var description: String? = description
+    var image: Int? = image
+}
